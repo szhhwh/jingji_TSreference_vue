d981e5761ba0c00ad9761eadc992ecc727555968
王浩 <3116737282@qq.com>
2024-11-09 08:40:32 +0800

    更细顶栏配置文件
    🐛配置修复
    1. 删除无用配置信息

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -5,23 +5,4 @@ export default navbar([
   "/reference/",
   "/spbook/",
   "/other/"
-  // {
-  //   text: "指南",
-  //   icon: "lightbulb",
-  //   prefix: "/guide/",
-  //   children: [
-  //     {
-  //       text: "Bar",
-  //       icon: "lightbulb",
-  //       prefix: "bar/",
-  //       children: ["baz", { text: "...", icon: "ellipsis", link: "" }],
-  //     },
-  //     {
-  //       text: "Foo",
-  //       icon: "lightbulb",
-  //       prefix: "foo/",
-  //       children: ["ray", { text: "...", icon: "ellipsis", link: "" }],
-  //     },
-  //   ],
-  // },
 ]);
