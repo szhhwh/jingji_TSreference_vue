0db3ea3a34d526f0f576e851542e4543f2e27752
王浩 <3116737282@qq.com>
2025-02-28 23:05:46 +0800

    更新高三特刊 1.1 节
    🔧Change 一般更改
    - 补充出血线规定

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -9,9 +9,35 @@ export default defineUserConfig({
   description: "荆棘鸟文学社官方参考文档",
 
   head: [
+    ["link", { rel: "icon", href: "favicon.ico" }],
     [
-      'link', { rel: 'icon', href: 'favicon.ico' }
-    ]
+      "script",
+      {},
+      `\
+(function() {
+  var script = document.createElement('script');
+  script.src = "https://lf-cdn.coze.cn/obj/unpkg/flow-platform/chat-app-sdk/1.1.0-beta.3/libs/cn/index.js";
+  script.onload = function() {
+    new CozeWebSDK.WebChatClient({
+      config: {
+        bot_id: '7352385184374767616',
+      },
+      componentProps: {
+        title: 'Coze',
+      },
+      auth: {
+        type: 'token',
+        token: 'pat_Xz8JkNbl5VLdVElhLr81ognIdFkObal62XIpq8TJBin3OEGe5tSUh5wCkUBLLTWP',
+        onRefreshToken: function () {
+          return 'pat_Xz8JkNbl5VLdVElhLr81ognIdFkObal62XIpq8TJBin3OEGe5tSUh5wCkUBLLTWP';
+        }
+      }
+    });
+  };
+  document.head.appendChild(script);
+})();
+      `,
+    ],
   ],
 
   theme,
